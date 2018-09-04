41463b28315e999a9d5b0f90cc55476ac88efeb3
James Hush <jhush91@gmail.com>
2018-09-03 22:02:20 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import RxPlayground from "./RxPlayground";
+// import RxPlayground from "./RxPlayground";
 import RxPlaygroundButtonClick from "./RxPlaygroundButtonClick";
 
 const App = () => {
   return (
     <Router>
       <React.Fragment>
-        <Route path="/rxjs" component={RxPlayground} />
-        <Route path="/rxjsButton" component={RxPlaygroundButtonClick} />
+        <Route path="/rxjs" component={RxPlaygroundButtonClick} />
       </React.Fragment>
     </Router>
   );
